419a58d1d160e8e020a7b8bf0caf2d56f36671fb
Florecita Wenny <h1101241039@student.untan.ac.id>
2026-02-25 04:43:44 +0000

    update

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(nama, umur) {
+    return 'selamat datang ' + nama + ' di PWL pada usia yang ke ' + umur + ' ';
+}
+const mahasiswa = "wanny";
+cont;
+umur: number = 19;
+console.log(hello(mahasiswa, umur));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function hello (nama: string, umur:number):string{
+    return 'selamat datang '+nama+' di PWL pada usia yang ke '+umur+' ';
+}
+
+const mahasiswa ="wanny";
+cont umur:number=19;
+console.log(hello(mahasiswa,umur));
